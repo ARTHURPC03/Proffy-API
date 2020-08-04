7cbdeaf1d1034821be7686716497218941c85751
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-08-04 12:47:45 -0300

    Config Eslint, Prettier and Editorconfig

---
+++ src/routes/index.ts
@@ -0,0 +1,6 @@
+// src/routes/index.ts
+import { Router } from 'express'
+
+const routes = Router()
+
+export default routes
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express'
+// src/server.ts
+import routes from './routes'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  
-  return response.json({ message: 'Hello World' })
-
-})
+app.get('/', (request, response) => response.json({ message: 'Hello World' }))
 
 app.listen(3333)
