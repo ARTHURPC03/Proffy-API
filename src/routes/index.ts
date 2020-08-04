df7d64d05c0082348f924d94904ceb9ddf698b78
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-08-04 20:46:10 -0300

    Finish

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,15 @@
-// src/routes/index.ts
 import { Router } from 'express'
+import ClassesController from '../controllers/ClassesController'
+import ConnectionsController from '../controllers/ConnectionsController'
 
 const routes = Router()
+const classesControllers = new ClassesController()
+const connectionsControllers = new ConnectionsController()
+
+routes.get('/classes', classesControllers.index)
+routes.post('/classes', classesControllers.create)
+
+routes.get('/connections', connectionsControllers.index)
+routes.post('/connections', connectionsControllers.create)
 
 export default routes
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
-// src/server.ts
+import cors from 'cors'
 import routes from './routes'
 
 const app = express()
 
-app.get('/', (request, response) => response.json({ message: 'Hello World' }))
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333)
